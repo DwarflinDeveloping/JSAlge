288684ce7927edf5610437eba37bafd66b8beb32
DwarfDev <74852537+DwarflinDeveloping@users.noreply.github.com>
2021-07-08 15:53:56 +0200

    Add files via upload

---
+++ alge.js
@@ -0,0 +1,16 @@
+function random_alge() {
+    var alge = alges[Math.floor(Math.random() * alges.length)];
+    return alge
+}
+
+
+function random_megaalge() {
+    var colour = megaalge_colours[Math.floor(Math.random() * megaalge_colours.length)];
+    return colour
+}
+
+
+function random_colour() {
+    var colour = alge_colours[Math.floor(Math.random() * alge_colours.length)];
+    return colour
+}
